555d2fbfb0bc7dc67d641c9a704482c3c2e4e63b
Bastian <bopatzable@gmail.com>
2019-11-22 13:31:36 +0100

    implemented Algorithm to extract JDL and HD from textfile

--- src/main.py
+++ src/main.py
@@ -28,16 +28,73 @@ import os
 
 # path = r"C:\\Users\\Divalu\\git\\RaPlo\\data\\testdaten1.txt"
 # print(path)
-jointDetecion = []
-#file = open("testdaten1.txt")
+
+dataRaw = []
+dataFinal = []
+
 with open("testdaten1.txt") as file:
-    #for line in file:
-    for index, line in enumerate(file):    
-        content = line.rstrip('\n')
-        #print(content)
-        if content:
-            print("Content: "+repr(content))
-            print(index)
+    #for line in file:        
+    #linesRaw=file.readlines()
+    for lineRaw in file:
+        line = lineRaw.rstrip('\n').strip()
+        #if line:
+        dataRaw.append(line)    
+    file.close()
+    
+      
+updatedIndex = 0        
+for index, d in enumerate(dataRaw):
+    print(repr(d))
+    if d:        
+        if "Joint" in d[:5]:
+            dataPair = {}            
+            updatedIndex = index + 1#             
+            JDLabel = dataRaw[updatedIndex][:-1].split(",")
+            updatedIndex = updatedIndex + 1 
+            JDLabelValuePairs = []                      
+            while dataRaw[updatedIndex]:
+                JDLabelValuePair = {}                
+                JDValues = dataRaw[updatedIndex][:-1].split(",")
+                for i, label in enumerate(JDLabel):
+                    JDLabelValuePair[label] = JDValues[i]
+                JDLabelValuePairs.append(JDLabelValuePair)                                                    
+                updatedIndex = updatedIndex + 1
+            dataPair["Joint Detection"] = JDLabelValuePairs
+                
+        if "Host" in d[:4]:
+            updatedIndex = index + 1#             
+            HDLabel = dataRaw[updatedIndex][:-1].split(",")
+            updatedIndex = updatedIndex + 1 
+            HDLabelValuePairs = []          
+            while not "</DGM>" in dataRaw[updatedIndex]:
+                HDLabelValuePair = {}                
+                HDValues = dataRaw[updatedIndex][:-1].split(",")
+                for i, label in enumerate(HDLabel):
+                    HDLabelValuePair[label] = HDValues[i]
+                HDLabelValuePairs.append(HDLabelValuePair)                                                    
+                updatedIndex = updatedIndex + 1
+            dataPair["Host Dynamics"] = HDLabelValuePairs
+            dataFinal.append(dataPair)      
+        
+                
+print(repr(dataFinal))
+                         
+                
+        
+            
+                   
+    
+    
+#     for index, line in enumerate(file):    
+#         content = line.rstrip('\n').strip()
+#         #print(content)
+#         if content:
+#             if content[-1] == ":":
+#                 #jdl = line[]
+#                 print("Content: "+repr(content))
+#                 print(index)
+                
+                
             #if "Joint Detection List:" in content:
 #                 header = file.readline()
 #                 jointDetecion.append(file.readline())
@@ -49,9 +106,7 @@ with open("testdaten1.txt") as file:
         #    hostDynamics = file.readline()
       
         #print(hostDynamics)
-    
-    
-    file.close() 
+
            
     
     
