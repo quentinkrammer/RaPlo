1a9eddad8a8cf003a583c5267461a03f8b1bbaa7
Bastian <bopatzable@gmail.com>
2019-11-25 16:04:03 +0100

    main.py

--- src/main.py
+++ src/main.py
@@ -6,51 +6,17 @@ from DeparseData import DeparseData
 from RTS_Plot import RTS_Plot
 
 
-# def writeToFile(txt):
-#     f = open(".\data\testdatenX.txt", "a")
-#     f.write(txt)
-#     f.close()
-# 
-# if __name__ == '__main__':
-#     
-# 
-#     HOST = '127.0.0.1'  # The server's hostname or IP address
-#     PORT = 1337        # The port used by the server
-#     
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#         s.connect((HOST, PORT))
-#         #s.sendall(b'Hello, world')
-#         while(True):
-#             data = s.recv(int(4.0E7))
-#             writeToFile(data.decode())
-#path = os.path.realpath('..')# + "\\data\\testdaten1.txt"
-
 parser = DeparseData("local")
 jdl, hd = parser.deparseNextSegment()
 print(jdl)
 print(len(jdl["Version"]))
 
-
 jdl, hd = parser.deparseNextSegment()
 print(jdl)
 jdl, hd = parser.deparseNextSegment()
 print(jdl)
 jdl, hd = parser.deparseNextSegment()
 print(jdl)
-
-
-# print(jdl)
-# print("----------------ENDE----------------")
-# jdl = parser.deparseNextSegment()
-# print(jdl)
-# 
-# print("----------------ENDE----------------")
-# jdl = parser.deparseNextSegment()
-# print(jdl)
-# 
-# print("----------------ende----------------")
-# jdl = parser.deparseNextSegment()
-# print(jdl)
 
 
 # rts = RTS_Plot()
